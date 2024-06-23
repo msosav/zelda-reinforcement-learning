c9739a77c69f6fd817e5eb3d1bc2cfd8840d89a5
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-06-23 16:26:41 -0500

    Add valid buttons and button press example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pyboy import PyBoy
 from pyboy.utils import WindowEvent
 
+valid_buttons = ["a", "b", "start", "select", "left", "right", "up", "down"]
+
 
 def load_state(path: str, pyboy: PyBoy) -> None:
     with open(path, "rb") as state:
@@ -13,6 +15,8 @@ if __name__ == "__main__":
     load_state("roms/ZeldaLinksAwakening.gb.state", pyboy)
 
     while pyboy.tick():
+        pyboy.button(valid_buttons[0])  # a
+        pyboy.tick()
         pass
 
     pyboy.stop()
