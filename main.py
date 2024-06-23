22e957110f4c0a95d8dfc0c05ac9ebadf91e61ee
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-06-23 16:17:46 -0500

    Add state loading

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from pyboy import PyBoy
+from pyboy.utils import WindowEvent
+
+
+def load_state(path: str, pyboy: PyBoy) -> None:
+    with open(path, "rb") as state:
+        pyboy.load_state(state)
+
 
 if __name__ == "__main__":
     pyboy = PyBoy("roms/ZeldaLinksAwakening.gb")
+
+    load_state("roms/ZeldaLinksAwakening.gb.state", pyboy)
 
     while pyboy.tick():
         pass
