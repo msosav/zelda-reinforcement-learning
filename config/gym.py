9b0725b3e4dabec285fffa14f5f05dacaf8ac18b
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-08-07 19:15:37 -0500

    Add reward logic for obtaining items

--- config/gym.py
+++ config/gym.py
@@ -31,6 +31,22 @@ class ZeldaGymEnv(gym.Env):
             low=0, high=255, shape=(16, 20), dtype=np.uint8)
         self.action_space = spaces.Discrete(len(self.valid_actions))
 
+        self.items = {
+            '01': False,  # Sword
+            '02': False,  # Bombs
+            '03': False,  # Power bracelet
+            '04': False,  # Shield
+            '05': False,  # Bow
+            '06': False,  # Hookshot
+            '07': False,  # Fire rod
+            '08': False,  # Pegasus boots
+            '09': False,  # Ocarina
+            '0A': False,  # Feather
+            '0B': False,  # Shovel
+            '0C': False,  # Magic powder
+            '0D': False  # Boomrang
+        }
+
     def step(self, action):
         assert self.action_space.contains(
             action), "%r (%s) invalid" % (action, type(action))
@@ -61,8 +77,9 @@ class ZeldaGymEnv(gym.Env):
     def _calculate_fitness(self):
         self._previous_fitness = self._fitness
 
-        # TODO: Implement reward logic
         self._fitness = 0
+
+        self._fitness += self._check_new_items()
 
     def reset(self, **kwargs):
         try:
@@ -83,3 +100,13 @@ class ZeldaGymEnv(gym.Env):
 
     def close(self):
         self.pyboy.stop()
+
+    def _check_new_items(self):
+        items_in_inventory_count = 0
+        for inventory_address in ADDR_INVENTORY:
+            item_in_inventory = self.pyboy.memory[inventory_address]
+            if item_in_inventory in self.items:
+                self.items[item_in_inventory] = True
+                items_in_inventory_count += 1
+
+        return items_in_inventory_count
--- config/memory_addresses.py
+++ config/memory_addresses.py
@@ -8,3 +8,18 @@ ADDR_POSITION_8X8 = 0xDBAE
 # 20: Owl talked, 80: Visited
 # For example, visiting the first dungeon's screen (80) and opening it with the key (10) would put that byte at 90
 ADDR_WORLD_MAP_STATUS = [i for i in range(0xD800, 0xD900)]
+
+"""01 	Sword
+02 	Bombs
+03 	Power bracelet
+04 	Shield
+05 	Bow
+06 	Hookshot
+07 	Fire rod
+08 	Pegasus boots
+09 	Ocarina
+0A 	Feather
+0B 	Shovel
+0C 	Magic powder
+0D 	Boomrang """
+ADDR_INVENTORY = [i for i in range(0xDB00, 0xDB0B)]  # Also contains held items
