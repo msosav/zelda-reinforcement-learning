2b4a63d231d46bf9886f05e2df691f1feba26ba1
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-08-25 13:35:25 -0500

    Implement preprocessing of the environment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from config.preprocess import Preprocess
+from config.preprocess import PreprocessEnv
 
 if __name__ == "__main__":
     config = {
@@ -6,14 +6,12 @@ if __name__ == "__main__":
         'state_path': 'roms/ZeldaLinksAwakening.gb.state'
     }
 
-    env = Preprocess(config)
-
+    env = PreprocessEnv(config)
     done = True
     for step in range(100000):
         if done:
             env.reset()
-        observation, reward, done, truncated, info = env.step(
-            env.action_space.sample())
-        print(observation.shape)
+        observation, reward, done, info = env.step(
+            [env.action_space.sample()])
         env.render()
     env.close()
