68c275037289d198565b860f4da671a2a3970ab4
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-08-25 15:49:06 -0500

    Implement PPO algorithm

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from config.preprocess import PreprocessEnv
+from utils import PreprocessEnv
+from utils import CheckpointAndLoggingCallback
+from stable_baselines3 import PPO
+import sys
 
 if __name__ == "__main__":
     config = {
         'rom_path': 'roms/ZeldaLinksAwakening.gb',
-        'state_path': 'roms/ZeldaLinksAwakening.gb.state'
+        'state_path': 'roms/ZeldaLinksAwakening.gb.state',
+        'checkpoint_dir': 'checkpoints/',
+        'log_dir': 'logs/',
     }
 
+    callback = CheckpointAndLoggingCallback(
+        check_freq=1000, save_path=config['checkpoint_dir'])
+
     env = PreprocessEnv(config)
-    done = True
-    for step in range(100000):
-        if done:
-            env.reset()
-        observation, reward, done, info = env.step(
-            [env.action_space.sample()])
-        env.render()
-    env.close()
+
+    mode = sys.argv[1]
+
+    if mode == 'train':
+        model = PPO('CnnPolicy', env, verbose=1,
+                    tensorboard_log=config['log_dir'], learning_rate=0.000001, n_steps=512)
+
+        model.learn(total_timesteps=1000000, callback=callback)
+    elif mode == 'test':
+        model = PPO.load('./checkpoints/best_model_22000')
+
+        observation = env.reset()
+
+        while True:
+            action, _state = model.predict(observation)
+            observation, reward, done, info = env.step(action)
+            env.render()
---
+++ utils.py
@@ -0,0 +1,44 @@
+import os
+from stable_baselines3.common.callbacks import BaseCallback  # For saving models
+
+from gymnasium.wrappers import gray_scale_observation
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from config.gym import ZeldaGymEnv
+
+
+def PreprocessEnv(config: dict) -> VecFrameStack:
+    """
+    Preprocesses the environment for reinforcement learning.
+
+    Args:
+        config (dict): Configuration parameters for the environment.
+
+    Returns:
+        VecFrameStack: Preprocessed environment with stacked frames.
+    """
+
+    env = ZeldaGymEnv(config, debug=True)
+    env = gray_scale_observation.GrayScaleObservation(env, keep_dim=True)
+    env = DummyVecEnv([lambda: env])
+    env = VecFrameStack(env, 4, channels_order='last')
+
+    return env
+
+
+class CheckpointAndLoggingCallback(BaseCallback):
+    def __init__(self, check_freq, save_path, verbose=0):
+        super(CheckpointAndLoggingCallback, self).__init__(verbose)
+        self.chek_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self) -> None:
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.chek_freq == 0:
+            model_path = os.path.join(
+                self.save_path, f"best_model_{self.n_calls}.zip")
+            self.model.save(model_path)
+
+        return True
